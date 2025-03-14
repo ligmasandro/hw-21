e80b7891bd1be5e367264b50b521a1169ae3f747
ligmasandro <sandrokashiashvili1@Gmail.com>
2025-03-14 12:35:46 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+const elem1 = document.querySelector(".bgreen")
+const elem2 = document.querySelector("body")
+const elem3 = document.querySelector(".bblue")
+const elem4 = document.querySelector(".bred")
+const elem5 = document.querySelector(".byellow")
+console.log(elem1);
+elem1.addEventListener("click",  () =>{
+    elem2.style.backgroundColor = "green"
+
+})
+elem3.addEventListener("click",  () =>{
+    elem2.style.backgroundColor = "blue"
+
+})
+elem4.addEventListener("click",  () =>{
+    elem2.style.backgroundColor = "red"
+
+})
+elem5.addEventListener("click",  () =>{
+    elem2.style.backgroundColor = "yellow"
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(elem1);
+console.log(elem2);
+console.log(elem3);
+console.log(elem4);
+console.log(elem5);
+
+
+
